2d19fcbe33b777bfb88da90f815784c0bf625559
tickbh <tickdream125@hotmail.com>
2022-04-01 10:43:10 +0800

    forever

--- src/ok.rs
+++ src/ok.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use proc_macro2::{Span, Ident};
+use proc_macro2::{Span};
 
 use quote::{quote, quote_spanned, ToTokens};
 use syn::parse::Parser;
@@ -11,22 +11,22 @@ fn token_stream_with_error(mut tokens: TokenStream, error: syn::Error) -> TokenS
 }
 
 struct FinalConfig {
-    worker_threads: Option<usize>,
+    reload_max_times: Option<usize>,
+    monitor_file: Option<String>,
 }
 
 const DEFAULT_ERROR_CONFIG: FinalConfig = FinalConfig {
-    worker_threads: None,
+    reload_max_times: None,
+    monitor_file: None,
 };
 
 struct Configuration {
-    worker_threads: Option<(usize, Span)>,
-    is_test: bool,
+    reload_max_times: Option<(usize, Span)>,
+    monitor_file: Option<(String, Span)>,
 }
 
 
 fn parse_int(int: syn::Lit, span: Span, field: &str) -> Result<usize, syn::Error> {
-    println!("int === {:?}", span);
-    println!("field === {:?}", field);
     match int {
         syn::Lit::Int(lit) => match lit.base10_parse::<usize>() {
             Ok(value) => Ok(value),
@@ -53,82 +53,81 @@ fn parse_string(int: syn::Lit, span: Span, field: &str) -> Result<String, syn::E
     }
 }
 
-fn parse_bool(bool: syn::Lit, span: Span, field: &str) -> Result<bool, syn::Error> {
-    match bool {
-        syn::Lit::Bool(b) => Ok(b.value),
-        _ => Err(syn::Error::new(
-            span,
-            format!("Failed to parse value of `{}` as bool.", field),
-        )),
-    }
-}
-
-
 impl Configuration {
-    fn new(is_test: bool) -> Self {
+    fn new() -> Self {
         Configuration {
-            worker_threads: None,
-            is_test,
+            reload_max_times: None,
+            monitor_file: None,
         }
     }
 
-
-    fn set_worker_threads(
+    fn set_reload_max_times(
         &mut self,
-        worker_threads: syn::Lit,
+        reload_max_times: syn::Lit,
         span: Span,
     ) -> Result<(), syn::Error> {
-        if self.worker_threads.is_some() {
+        if self.reload_max_times.is_some() {
             return Err(syn::Error::new(
                 span,
-                "`worker_threads` set multiple times.",
+                "`reload_max_times` set multiple times.",
             ));
         }
 
-        let worker_threads = parse_int(worker_threads, span, "worker_threads")?;
-        if worker_threads == 0 {
-            return Err(syn::Error::new(span, "`worker_threads` may not be 0."));
+        let reload_max_times = parse_int(reload_max_times, span, "reload_max_times")?;
+        if reload_max_times == 0 {
+            return Err(syn::Error::new(span, "`reload_max_times` may not be 0."));
         }
-        self.worker_threads = Some((worker_threads, span));
+        self.reload_max_times = Some((reload_max_times, span));
+        Ok(())
+    }
+    
+    fn set_monitor_file(
+        &mut self,
+        monitor_file: syn::Lit,
+        span: Span,
+    ) -> Result<(), syn::Error> {
+        if self.monitor_file.is_some() {
+            return Err(syn::Error::new(
+                span,
+                "`monitor_file` set multiple times.",
+            ));
+        }
+
+        let monitor_file = parse_string(monitor_file, span, "reload_max_times")?;
+        if monitor_file == String::new() {
+            return Err(syn::Error::new(span, "`monitor_file` may not be empty."));
+        }
+        self.monitor_file = Some((monitor_file, span));
         Ok(())
     }
 
-    fn macro_name(&self) -> &'static str {
-        if self.is_test {
-            "forever::test"
-        } else {
-            "forever::main"
-        }
-    }
-
     fn build(&self) -> Result<FinalConfig, syn::Error> {
-
-        let worker_threads = match self.worker_threads {
+        let reload_max_times = match self.reload_max_times {
             None => None,
             Some((value, _)) => {
                 Some(value)
             }
         };
+        
+        let monitor_file = match &self.monitor_file {
+            None => None,
+            Some((value, _)) => {
+                Some(value.clone())
+            }
+        };
 
         Ok(FinalConfig {
-            worker_threads,
+            reload_max_times,
+            monitor_file,
         })
     }
 }
 
 fn build_config(
-    input: syn::ItemFn,
+    _input: syn::ItemFn,
     args: AttributeArgs,
-    is_test: bool,
 ) -> Result<FinalConfig, syn::Error> {
-    // if input.sig.asyncness.is_none() {
-    //     let msg = "the `async` keyword is missing from the function declaration";
-    //     return Err(syn::Error::new_spanned(input.sig.fn_token, msg));
-    // }
-
-    let mut config = Configuration::new(is_test);
-    let macro_name = config.macro_name();
-
+    let mut config = Configuration::new();
     for arg in args {
         match arg {
             syn::NestedMeta::Meta(syn::Meta::NameValue(namevalue)) => {
@@ -141,48 +140,26 @@ fn build_config(
                     .to_string()
                     .to_lowercase();
                 match ident.as_str() {
-                    "worker_threads" => {
-                        config.set_worker_threads(
+                    "max_times" => {
+                        config.set_reload_max_times(
+                            namevalue.lit.clone(),
+                            syn::spanned::Spanned::span(&namevalue.lit),
+                        )?;
+                    }
+                    "monitor_file" => {
+                        config.set_monitor_file(
                             namevalue.lit.clone(),
                             syn::spanned::Spanned::span(&namevalue.lit),
                         )?;
                     }
                     name => {
                         let msg = format!(
-                            "Unknown attribute {} is specified; expected one of: `flavor`, `worker_threads`, `start_paused`",
+                            "Unknown attribute {} is specified; expected one of: `flavor`, `reload_max_times`, `start_paused`",
                             name,
                         );
                         return Err(syn::Error::new_spanned(namevalue, msg));
                     }
                 }
-            }
-            syn::NestedMeta::Meta(syn::Meta::Path(path)) => {
-                let name = path
-                    .get_ident()
-                    .ok_or_else(|| syn::Error::new_spanned(&path, "Must have specified ident"))?
-                    .to_string()
-                    .to_lowercase();
-                let msg = match name.as_str() {
-                    "threaded_scheduler" | "multi_thread" => {
-                        format!(
-                            "Set the runtime flavor with #[{}(flavor = \"multi_thread\")].",
-                            macro_name
-                        )
-                    }
-                    "basic_scheduler" | "current_thread" | "single_threaded" => {
-                        format!(
-                            "Set the runtime flavor with #[{}(flavor = \"current_thread\")].",
-                            macro_name
-                        )
-                    }
-                    "flavor" | "worker_threads" | "start_paused" => {
-                        format!("The `{}` attribute requires an argument.", name)
-                    }
-                    name => {
-                        format!("Unknown attribute {} is specified; expected one of: `flavor`, `worker_threads`, `start_paused`", name)
-                    }
-                };
-                return Err(syn::Error::new_spanned(path, msg));
             }
             other => {
                 return Err(syn::Error::new_spanned(
@@ -197,11 +174,8 @@ fn build_config(
 }
 
 
-fn parse_knobs(mut input: syn::ItemFn, is_test: bool, config: FinalConfig) -> TokenStream {
-    // input.sig.asyncness = None;
-
-    // // If type mismatch occurs, the current rustc points to the last statement.
-    let (last_stmt_start_span, last_stmt_end_span) = {
+fn parse_knobs(mut input: syn::ItemFn, config: FinalConfig) -> TokenStream {
+    let (_last_stmt_start_span, last_stmt_end_span) = {
         let mut last_stmt = input
             .block
             .stmts
@@ -209,106 +183,118 @@ fn parse_knobs(mut input: syn::ItemFn, is_test: bool, config: FinalConfig) -> To
             .map(ToTokens::into_token_stream)
             .unwrap_or_default()
             .into_iter();
-        // `Span` on stable Rust has a limitation that only points to the first
-        // token, not the whole tokens. We can work around this limitation by
-        // using the first/last span of the tokens like
-        // `syn::Error::new_spanned` does.
         let start = last_stmt.next().map_or_else(Span::call_site, |t| t.span());
         let end = last_stmt.last().map_or(start, |t| t.span());
         (start, end)
     };
-    let worker_threads = config.worker_threads.unwrap_or(0);
+    let reload_max_times = config.reload_max_times.unwrap_or(9999999);
+    let monitor_file_name = config.monitor_file.unwrap_or(String::new());
     let body = &input.block;
+    
+    let (tail_return, tail_semicolon) = match body.stmts.last() {
+        Some(syn::Stmt::Semi(syn::Expr::Return(_), _)) => (quote! { return }, quote! { ; }),
+        Some(syn::Stmt::Semi(..)) | Some(syn::Stmt::Local(..)) | None => {
+            match &input.sig.output {
+                syn::ReturnType::Type(_, ty) if matches!(&**ty, syn::Type::Tuple(ty) if ty.elems.is_empty()) =>
+                {
+                    (quote! {}, quote! { ; }) // unit
+                }
+                syn::ReturnType::Default => (quote! {}, quote! { ; }), // unit
+                syn::ReturnType::Type(..) => (quote! {}, quote! {}),   // ! or another
+            }
+        }
+        _ => (quote! {}, quote! {}),
+    };
+
     input.block = syn::parse2(quote_spanned! {last_stmt_end_span=>
-        {
-            let command = ::commander::Commander::new()
-            .usage_desc("Forever Write by Rust")
-            .option("-ff, --fromforever [value]", "fromforever ", Some(false))
-            .option_str("-fm, --monitor [value]", "monitor file change for update", None)
-            .helps(vec!["fh".to_string(), "forever-help".to_string()])
-            .versions(vec!["fv".to_string(), "forever-version".to_string()])
-            .after_desc("\n\n Forever run in rust\n\n")
-            .parse_env_or_exit();
-
-            let args = env::args();
-            let mut list = command.get_all_args();
-            
-            println!("command.get_exec() == {:?}", command.get_exec());
-            println!("command.get_all_args() == {:?}", command.get_all_args());
-
-            println!("111 == {:?}", command.get("fromforever"));
-            let ssss = format!("command.get_exec() == {:?} command.get_all_args() == {:?}", command.get_exec(), command.get_all_args());
-            
-            if !command.get("fromforever").unwrap_or(false) {
-                list.push("--fromforever".to_string());
-                loop {
-                    let mut now = ::std::time::SystemTime::now();
-                    let mut child = if cfg!(target_os = "windows") {
-                        Command::new(command.get_exec().unwrap())
-                                .args(list.clone())
-                                .stdin(Stdio::null())
-                                .stdout(Stdio::inherit())
-                                .spawn()
-                                .expect("failed to execute process")
-                    } else {
-                        Command::new(command.get_exec().unwrap())
-                                .args(list.clone())
-                                .stdin(Stdio::null())
-                                .stdout(Stdio::inherit())
-                                .spawn()
-                                .expect("failed to execute process")
-                    };
-                    println!("hello = {:?}", child);
-                    println!("args = {:?}", list);
-                    
+        {   
+            {
+                let command = ::commander::Commander::new()
+                .usage_desc("Forever Write by Rust")
+                .option("-ff, --fromforever [value]", "fromforever ", Some(false))
+                .option_str("-fm, --monitor [value]", "monitor file change for update", None)
+                .helps(vec!["fh".to_string(), "forever-help".to_string()])
+                .versions(vec!["fv".to_string(), "forever-version".to_string()])
+                .after_desc("\n\n Forever run in rust\n\n")
+                .parse_env_or_exit();
+    
+                let mut reload_left_times = #reload_max_times;
+                let mut monitor_file = #monitor_file_name.to_string();
+                if let Some(monitor) = command.get_str("monitor") {
+                    monitor_file = monitor;
+                }
+    
+                if !command.get("fromforever").unwrap_or(false) {
+                    let mut list = command.get_all_args();
+                    list.push("--fromforever".to_string());
                     loop {
-                        
-                        match child.try_wait() {
-                            Ok(Some(status)) => {println!("exited with: {}", status);break;},
-                            Ok(None) => {
-                                println!("status not ready yet, let's really wait");
-                            }
-                            Err(e) => {println!("error attempting to wait: {}", e); break;},
+                        reload_left_times-=1;
+                        if reload_left_times <= 0 {
+                            break
                         }
-                        ::std::thread::sleep_ms(1000);
-                        if let Some(file) = command.get_str("monitor") {
-                            // println!("ffffffffffff {:?}", ::std::fs::metadata(file));
-                            if let Some(metadata) = ::std::fs::metadata(file).ok() {
-                                println!("metadata == {:?}", metadata);
-                                if let Ok(time) = metadata.modified() {
-                                    let result = time.cmp(&now);
-                                    println!("time === {:?} now == {:?}", time, now);
-                                    println!("result === {:?}", result);
-                                    if result == ::std::cmp::Ordering::Greater {
-                                        child.kill();
-                                        println!("killed!!!!!");
-                                        break;
+    
+                        let mut now = ::std::time::SystemTime::now();
+                        let mut child = if cfg!(target_os = "windows") {
+                            ::std::process::Command::new(command.get_exec().unwrap())
+                                    .args(list.clone())
+                                    .stdin(::std::process::Stdio::null())
+                                    .stdout(::std::process::Stdio::inherit())
+                                    .spawn()
+                                    .expect("failed to execute process")
+                        } else {
+                            ::std::process::Command::new(command.get_exec().unwrap())
+                                    .args(list.clone())
+                                    .stdin(::std::process::Stdio::null())
+                                    .stdout(::std::process::Stdio::inherit())
+                                    .spawn()
+                                    .expect("failed to execute process")
+                        };
+    
+                        
+                        loop {
+                            
+                            match child.try_wait() {
+                                Ok(Some(status)) => {println!("exited with: {}", status);break;},
+                                Ok(None) => {
+                                    println!("status not ready yet, let's really wait");
+                                }
+                                Err(e) => {println!("error attempting to wait: {}", e); break;},
+                            }
+                            ::std::thread::sleep(::std::time::Duration::from_millis(1000));
+                            if monitor_file != String::new() {
+                                if let Some(metadata) = ::std::fs::metadata(&monitor_file).ok() {
+                                    println!("metadata == {:?}", metadata);
+                                    if let Ok(time) = metadata.modified() {
+                                        let result = time.cmp(&now);
+                                        if result == ::std::cmp::Ordering::Greater {
+                                            child.kill();
+                                            break;
+                                        }
                                     }
                                 }
-                            }
-                            println!("aaaaaaaaaa");
-                        } 
+                            } 
+                        }
                     }
+                    return
                 }
             }
 
-            eprintln!("------?????????????---------- {:?}", #worker_threads);
-            #body
+            let _body = {
+                #body
+            }
+            // #[allow(clippy::expect_used)]
+            #tail_return #tail_semicolon
         }
     }).expect("Parsing failure");
 
     let result = quote! {
-        use std::io::prelude::*;
         #input
     };
-    println!("result == {:?}", result);
     result.into()
 }
 
 // #[cfg(not(test))] // Work around for rust-lang/rust#62127
 pub fn main(args: TokenStream, item: TokenStream) -> TokenStream {
-    println!("args = {:?}", args);
-    println!("input = {:?}", item);
 
     let input: syn::ItemFn = match syn::parse(item.clone()) {
         Ok(it) => it,
@@ -321,13 +307,12 @@ pub fn main(args: TokenStream, item: TokenStream) -> TokenStream {
     } else {
         AttributeArgs::parse_terminated
             .parse(args)
-            .and_then(|args| build_config(input.clone(), args, false))
+            .and_then(|args| build_config(input.clone(), args))
     };
 
-    
     match config {
-        Ok(config) => parse_knobs(input, false, config),
-        Err(e) => token_stream_with_error(parse_knobs(input, false, DEFAULT_ERROR_CONFIG), e),
+        Ok(config) => parse_knobs(input, config),
+        Err(e) => token_stream_with_error(parse_knobs(input, DEFAULT_ERROR_CONFIG), e),
     }
 
 
